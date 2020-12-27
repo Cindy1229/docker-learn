86ced5245c61633e16690da404043206b9a61603
Cindy1229 <leticiashi3@gmail.com>
2020-12-26 22:03:53 -0500

    first

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:13-alpine
+FROM node:alpine3.11
 
 ENV MONGO_DB_USERNAME=admin \
     MONGO_DB_PWD=password
@@ -7,7 +7,7 @@ RUN mkdir -p /home/app
 
 COPY ./app /home/app
 
-# set default dir so that next commands executes in /home/app dir
+#set default dir so that next commands executes in /home/app dir
 WORKDIR /home/app
 
 # will execute npm install in /home/app because of WORKDIR
--- app/server.js
+++ app/server.js
@@ -11,11 +11,12 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
+    console.log(__dirname)
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
 app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+  let img = fs.readFileSync(path.join(__dirname, "images/profile-2.jpg"));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
@@ -32,7 +33,7 @@ app.post('/update-profile', function (req, res) {
   MongoClient.connect(mongoUrlLocal, function (err, client) {
     if (err) throw err;
 
-    let db = client.db('my-db');
+    let db = client.db('user-account');
     userObj['userid'] = 1;
 
     let myquery = { userid: 1 };
@@ -54,7 +55,7 @@ app.get('/get-profile', function (req, res) {
   MongoClient.connect(mongoUrlLocal, function (err, client) {
     if (err) throw err;
 
-    let db = client.db('my-db');
+    let db = client.db('user-account');
 
     let myquery = { userid: 1 };
 
